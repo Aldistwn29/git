2ad4bc01a77a5b2465df94fc661704d9c30e15f5
Aldi setiawan <aldi60051@gmail.com>
2024-06-25 00:53:17 +0700

    latihan

--- ngulikData.py
+++ ngulikData.py
@@ -1,10 +1,27 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 # data
 data = pd.DataFrame({
-    'nama' : ['aldi', 'egel', 'purnama'],
-    'usia' : [20, 21, 22]
+    'nama' : ['aldi', 'egel', 'purnama', 'rudi', 'toni', 'budi'],
+    'usia' : [20, 21, 22, 23, 24, 25],
+    'pendidikan' : ['S1', 'S2', 'S3', 'S1', 'S2', 'S3'],
+    'status' : ['belum menikah', 'menikah', 'belum menikah', 'menikah', 'belum menikah', 'menikah'],
+    'pendapatan' : [1000000, 2000000, 3000000, 1500000, 2500000, 3500000]
 })
 
-print("Data: ",data)
+
+
+# visualisasi
+# fig, ax = plt.subplots()
+# ax.bar(data['pendidikan'], data['pendapatan'], color=color)
+# ax.set_xlabel('Pendidikan')
+# ax.set_ylabel('pendapatan')
+# ax.set_title('Pendapatan Berdasarakan Pendidikan')
+# plt.show()
+
+# visuliasi dengan seaborn
+import seaborn as sns
+sns.barplot(data, x='pendidikan', y='pendapatan',hue='pendidikan')
+plt.show()
---
+++ peta.py
@@ -0,0 +1,4 @@
+import folium 
+
+m = folium.Map(location=(-2.5, 118.0), zoom_start=8)
+m.save('indonesia.html')
